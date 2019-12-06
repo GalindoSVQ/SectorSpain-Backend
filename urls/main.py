c73607e6e5cd74b3576a856966018b9e828df983
Antonio Galindo <galindosvq@gmail.com>
2019-12-06 23:58:20 +0100

    url fixed

--- urls/main.py
+++ urls/main.py
@@ -25,40 +25,48 @@ from django.conf import settings
 from django.conf.urls import handler404, handler500
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Django API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="admin@z1.digital"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Django API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="admin@z1.digital"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 
-required_urlpatterns = [
+# required_urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('auth/', include(('users.rest_urls.auth', 'auth'), namespace='auth')),
+#     path('users/', include(('users.rest_urls.users', 'users'), namespace='users')),
+#     path('api/', include(('apps.climb.urls', 'climb'), namespace='climb-api')),
+# ]
+
+
+# swagger_urlpatterns = [
+#     path('', schema_view.with_ui('swagger', cache_timeout=0),
+#          name='schema-swagger-ui'),
+#     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+#             schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#     path('redoc/', schema_view.with_ui('redoc',
+#                                        cache_timeout=0), name='schema-redoc'),
+# ]
+
+
+# urlpatterns = ([]
+#                + required_urlpatterns
+#                + swagger_urlpatterns
+#                + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#                + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+
+
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include(('users.rest_urls.auth', 'auth'), namespace='auth')),
     path('users/', include(('users.rest_urls.users', 'users'), namespace='users')),
     path('api/', include(('apps.climb.urls', 'climb'), namespace='climb-api')),
 ]
-
-
-swagger_urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('redoc/', schema_view.with_ui('redoc',
-                                       cache_timeout=0), name='schema-redoc'),
-]
-
-
-urlpatterns = ([]
-               + required_urlpatterns
-               + swagger_urlpatterns
-               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
