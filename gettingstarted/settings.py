a9aa8e8d10305eb30422bffb5773ca96fe615135
Antonio Galindo <galindosvq@gmail.com>
2019-12-06 22:47:17 +0100

    django key

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,7 +27,7 @@ load_dotenv(dotenv_path)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
