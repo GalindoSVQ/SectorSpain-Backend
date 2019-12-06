fccfacaee2dc352f166d914fb04f850c579e5aa5
Antonio Galindo <galindosvq@gmail.com>
2019-12-06 22:36:27 +0100

    Static Files Adddes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -177,15 +177,12 @@ SWAGGER_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '../static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
-
+django_heroku.settings(locals())
 # Celery
 CELERY_TIMEZONE = 'US/Eastern'
 CELERYD_TASK_TIME_LIMIT = 700
