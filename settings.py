9725140d6f28b2de5193d53ae662bf6db7bdd711
Antonio Galindo <galindosvq@gmail.com>
2019-12-06 23:22:22 +0100

    Debug True

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
