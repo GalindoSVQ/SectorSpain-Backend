ca072981a795be2c65f89d2e0d5efde79fad4aca
Antonio Galindo <galindosvq@gmail.com>
2019-12-06 22:17:49 +0100

    Added timedelta

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-
+from os.path import join, dirname
+from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
