8e8ae7ecc83627600dcd6f4d291e97eae2777544
Antonio Galindo <galindosvq@gmail.com>
2019-12-07 01:31:47 +0100

    Static Files

--- settings.py
+++ settings.py
@@ -164,9 +164,8 @@ SWAGGER_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, '../static_root')
-#STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static_root')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
